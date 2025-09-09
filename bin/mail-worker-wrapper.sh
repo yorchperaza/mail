1a448d132dee240412744af25078f8475ddbd279
Jorge Peraza <jorge@monkeys.cloud>
2025-09-09 13:00:00 -0600

    mail worker

--- bin/mail-worker-wrapper.sh
+++ bin/mail-worker-wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # bin/mail-worker-wrapper.sh
 
-# Set environment variables
-export REDIS_HOST=10.0.0.164
+# Use localhost if Redis is on the same server
+export REDIS_HOST=127.0.0.1
 export REDIS_PORT=6379
 export REDIS_DB=0
 export REDIS_AUTH=S3cureRedisPa55!
-export REDIS_URL="redis://:S3cureRedisPa55!@10.0.0.164:6379/0"
+export REDIS_URL="redis://:S3cureRedisPa55!@127.0.0.1:6379/0"
 
 export SMTP_HOST=smtp.monkeysmail.com
 export SMTP_PORT=587
